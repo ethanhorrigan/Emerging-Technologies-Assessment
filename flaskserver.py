64cf44c5c38af586974f6f92bafac7a69aa582f5
EthanHorrigan <ethanhorr@gmail.com>
2019-10-10 21:14:15 +0100

    rename server class

--- flaskserver.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('view/html/frontend.html')
-    
-if __name__ == '__main__':
-    app.run(debug=True)
