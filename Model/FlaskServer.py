38562fa7fa4bd968eb9e8520fedd405ab00f455b
EthanHorrigan <ethanhorr@gmail.com>
2019-12-11 17:52:49 +0000

    Changed img.point

--- Model/FlaskServer.py
+++ Model/FlaskServer.py
@@ -40,26 +40,35 @@ def predict():
     
     with open("image.png", "wb") as f:
         f.write(decode)
-
+    
+    # Defining the width and height of the image resize
     width = 28
     height = 28
+    # Dimensions = Width x Height
     dim = (width, height)
     
-    # img = Image.open("image.png")
-    img = Image.open("image.png").convert('L')
+    img = Image.open("image.png")
+    # img = Image.open("image.png").convert('L')
+    # Save the Original Image (For Comparison)
     img.save("OriginalImage.png")
 
     # Downsize the Image to 28 x 28
     # https://stackoverflow.com/questions/273946/how-do-i-resize-an-image-using-pil-and-maintain-its-aspect-ratio
     img = img.resize(dim, Image.ANTIALIAS)
+    img.save("ResizedImage.png")
     
+    # Old Resize Method, did not work as intended so I opted for the PIL libray instead.
     # https://www.tutorialkart.com/opencv/python/opencv-python-resize-image/
     # img = cv2.resize(cv2.UMat(img), dsize=(28, 28), interpolation=cv2.INTER_NEAREST)
 
     # https://www.geeksforgeeks.org/python-pil-image-point-method/
-
+    # http://effbot.org/imagingbook/introduction.htm
+    # img point is used to try and preserve the edges and lines for more accurate predictions
+    
     threshold = 0
-    img = img.point(lambda p: p > threshold and 255)
+    # img = img.point(lambda p: p > threshold and 255)
+    pt = lambda p: p > threshold and 255
+    img = img.convert('L').point(pt)
     # Save the image in bytes.
     img.save("image.png")
     # Use openCV to read in the image.
@@ -67,13 +76,13 @@ def predict():
     # Grayscale the image.
 
     # Flatten (make one dimensional) and reshape the array without changing it's data.
-    # Convert the data to float so we can divide it by 255
+    # Convert the data to float or int so we can divide it by 255
     # Dividing by 255 will give us either a 1 or a 0.
     # 1 represents a drawn pixel.
     # 0 represents a pixel that has not been drawn on.
     # https://www.geeksforgeeks.org/differences-flatten-ravel-numpy/
     # grayArray = np.ndarray.ravel(np.array(gray)).reshape(1, 784).astype("float32") / 255
-    grayArray = np.ndarray.flatten(np.array(img)).reshape(1, 784).astype("uint8") / 255
+    grayArray = np.ndarray.ravel(np.array(img)).reshape(1, 784).astype("uint8") / 255
     #grayArray = ~np.ravel(gray).reshape(1, 784).astype(np.uint8) / 255.0
 
     # print("Printing image to array")
